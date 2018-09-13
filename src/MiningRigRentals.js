aeabd1dc45d3b75189e6f0302a9065335e1f7a3d
orpheus <ryanachacon@gmail.com>
2018-09-13 14:50:23 -0700

    getRig and getRental methods

--- src/MiningRigRentals.js
+++ src/MiningRigRentals.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import crypto from 'crypto';
-import querystring from 'querystring';
 
 /**
  An importable Javascript class to make REST requests to the MiningRigRentals API
@@ -23,7 +22,7 @@ class MiningRigRentals {
 			this.prevNonce = (new Date().getTime())
 		}
 	}
-	//Information API
+	/* ------------ Information API ----------- */
 	/**
 	 * Test connectivity and return information about you
 	 * @returns {Promise<Object>}
@@ -90,6 +89,62 @@ class MiningRigRentals {
 			throw this.createError(endpoint, 'GET', err)
 		}
 	};
+	/* ------------ Account API ----------- */
+	//@ToDO: add v1 account API methods
+
+	/* ------------ Rig API ----------- */
+	//@ToDo: finish getRig jsdoc (add the rest of the parameters)
+	/**
+	 *
+	 * @param  {object} options - input fields/query parameters
+	 * @param {string} options.type - Rig type, eg: sha256, scrypt, x11, etc
+	 * @param
+	 * @returns {Promise<*>}
+	 */
+	async getRig(options) {
+		let endpoint = '/rig';
+		let params = {};
+		if (options) {
+			for (let opt in options) {
+				params[opt] = options[opt]
+			}
+		}
+		let api = this.initAPI(endpoint, params);
+		try {
+			return (await api.get(endpoint)).data;
+		} catch (err) {
+			throw this.createError(endpoint, 'GET', err)
+		}
+	}
+	/* ------------ Rental API ----------- */
+	/**
+	 * Lists rentals
+	 * @param {Object} [options] - input fields/query parameters
+	 * @param {string} [options.type=renter] - Type is one of [owner,renter] -- owner means rentals on your rigs, renter means rentals you purchased
+	 * @param {string} [options.algo] - Filter by algo, see /info/algos
+	 * @param {boolean} [options.history=false] - true = Show completed rentals, false = Active rentals
+	 * @param {number} [options.rig] - Show rentals related to a specific rig ID
+	 * @param {number} [options.start=0] - Start number (for pagination)
+	 * @param {number} [options.limit=25] - Limit number (for pagination)
+	 * @returns {Promise<*>}
+	 */
+	async getRental(options) {
+		let endpoint = '/rental';
+		let params = {};
+		if (options) {
+			for (let opt in options) {
+				params[opt] = options[opt]
+			}
+		}
+		let api = this.initAPI(endpoint, params);
+		try {
+			return (await api.get(endpoint)).data;
+		} catch (err) {
+			throw this.createError(endpoint, 'GET', err)
+		}
+	}
+
+	/* ------------ AXIOS INITIATION ----------- */
 	/**
 	 * Initialize a new instance of axios with desired endpoint
 	 * @param {string} endpoint - the endpoint you wish to hit WITHOUT THE TRAILING SLASH; ex: /rig/14
