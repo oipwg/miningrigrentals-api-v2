5051c351d93fb5a12b95c072d0c529dad407fc33
orpheus <ryanachacon@gmail.com>
2018-09-13 14:50:05 -0700

    change apiKey

--- tests/MiningRigRentals.test.js
+++ tests/MiningRigRentals.test.js
@@ -1,30 +1,26 @@
 import MiningRigRentals from '../src/MiningRigRentals'
-
-const apiSettings = {
-	key: '731cf0f318903cd1b24b4fb025fd246082d20561ae613665fa14fc0c7112ba39',
-	secret: 'c4ebb2e8670ae7ae1c946da83a927463a64d02ad037d3296364464198e1bf80a'
-};
-const testEndpoint = '/rig/14';
+import apiKey from './apikey';
 
 describe('MiningRigRentals', () => {
 	/* ---------------- AXIOS ---------------- */
 	describe('Create an API axios instance', () => {
+		const testEndpoint = '/rig/14';
 		it('should generate a nonce that increases with each call', () => {
-			let mrr = new MiningRigRentals(apiSettings);
+			let mrr = new MiningRigRentals(apiKey);
 			let nonce = mrr.generateNonce();
 			let nonce2 = mrr.generateNonce();
 			expect(nonce2 > nonce).toBeTruthy();
 		});
 		it('should successfully build the HMAC signature', () => {
-			let mrr = new MiningRigRentals(apiSettings);
+			let mrr = new MiningRigRentals(apiKey);
 			//ToDo: test x-api-sign
 			console.log(mrr.createHMACSignature(testEndpoint, mrr.generateNonce()));
 		});
 		it('should build an axios instance with all the required mrr fields', () => {
-			let mrr = new MiningRigRentals(apiSettings);
+			let mrr = new MiningRigRentals(apiKey);
 			let api = mrr.initAPI(testEndpoint);
 			let headers = api.defaults.headers;
-			expect(headers['x-api-key']).toEqual(apiSettings.key);
+			expect(headers['x-api-key']).toEqual(apiKey.key);
 			expect(mrr.prevNonce === headers['x-api-nonce']).toBeTruthy();
 			//@ToDO: test x-api-sign
 		});
@@ -32,9 +28,10 @@ describe('MiningRigRentals', () => {
 	/* ------------ Information API ----------- */
 	describe('Information API', () => {
 		it('should make a GET call to /whoami', async () => {
-			let mrr = new MiningRigRentals(apiSettings), thrown = false;
+			let mrr = new MiningRigRentals(apiKey), thrown = false;
 			try {
 				let res = await mrr.whoami();
+				console.log(res)
 				expect(res.success).toBeTruthy()
 			} catch (err) {
 				thrown = true;
@@ -42,7 +39,7 @@ describe('MiningRigRentals', () => {
 			}
 		});
 		it('should make a GET call to /info/servers', async () => {
-			let mrr = new MiningRigRentals(apiSettings), thrown = false;
+			let mrr = new MiningRigRentals(apiKey), thrown = false;
 			try {
 				let res = await mrr.getServers();
 				expect(res.success).toBeTruthy()
@@ -52,7 +49,7 @@ describe('MiningRigRentals', () => {
 			}
 		});
 		it('should make a GET call to /info/algos', async () => {
-			let mrr = new MiningRigRentals(apiSettings), thrown = false;
+			let mrr = new MiningRigRentals(apiKey), thrown = false;
 			try {
 				let res = await mrr.getAlgos();
 				expect(res.success).toBeTruthy()
@@ -62,7 +59,7 @@ describe('MiningRigRentals', () => {
 			}
 		});
 		it('GET call to /info/algos with specified currency', async () => {
-			let mrr = new MiningRigRentals(apiSettings)
+			let mrr = new MiningRigRentals(apiKey)
 			let thrown = false, currency = 'EOS';
 			try {
 				let res = await mrr.getAlgos(currency);
@@ -75,7 +72,7 @@ describe('MiningRigRentals', () => {
 			}
 		}, 10000);
 		it('should make a GET call to /info/algo/[NAME]', async () => {
-			let mrr = new MiningRigRentals(apiSettings), thrown = false;
+			let mrr = new MiningRigRentals(apiKey), thrown = false;
 			try {
 				let res = await mrr.getAlgo("scrypt");
 				expect(res.success).toBeTruthy()
@@ -85,7 +82,7 @@ describe('MiningRigRentals', () => {
 			}
 		});
 		it('make a GET call to /info/algo/[NAME] with specified currency', async () => {
-			let mrr = new MiningRigRentals(apiSettings)
+			let mrr = new MiningRigRentals(apiKey)
 			let thrown = false, currency = 'DASH';
 			try {
 				let res = await mrr.getAlgo("scrypt", currency);
@@ -99,24 +96,40 @@ describe('MiningRigRentals', () => {
 	});
 	/* ------------ Account API ----------- */
 	describe('Account API', () => {
-		// it('should make a get call to /account', async () => {
-		// 	let mrr = new MiningRigRentals(apiSettings);
+		// it('should mathrown = true;
+		// 				expect(thrown).toBeTruthy()ke a get call to /account', async () => {
+		// 	let mrr = new MiningRigRentals(apiKey);
 		// 	let success = (await mrr.getAccount().success);
 		// 	expect(success).toBeFalsy()
 		// });
 	});
 	/* ------------ Rig API ----------- */
 	describe('Rig API', () => {
-		// it('should make a get call to /rig', async () => {
-		// 	let mrr = new MiningRigRentals(apiSettings);
-		// 	console.log(await mrr.getRig())
-		// })
+		it('should make a get call to /rig', async () => {
+			let mrr = new MiningRigRentals(apiKey);
+			let thrown = false;
+			try {
+				let res = await mrr.getRig({type: 'scrypt'});
+				expect(res.success).toBeTruthy()
+			} catch (err) {
+				thrown = true;
+				expect(thrown).toBeTruthy()
+			}
+		})
 	});
 	/* ------------ Rental API ----------- */
 	describe('Rental API', () => {
-		// it('should make a get call to /rental', async () => {
-		// 	let mrr = new MiningRigRentals(apiSettings);
-		// 	console.log(await mrr.getRental())
-		// })
+		it('should make a get call to /rental', async () => {
+			let mrr = new MiningRigRentals(apiKey);
+			let thrown = false;
+			try {
+				let res = await mrr.getRental();
+				console.log(res)
+				expect(res.success).toBeTruthy()
+			} catch (err) {
+				thrown = true;
+				expect(thrown).toBeTruthy()
+			}
+		})
 	})
 });
